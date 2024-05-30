c59be126969f444cea7d5976e5a4c775541fe940
Hamish <24396@students.mrgs.school.nz>
2024-05-30 13:31:09 +1200

    added notifications class

--- notifications.py
+++ notifications.py
@@ -1,8 +1,14 @@
 class PushNotifications:
     def __init__(self):
         self.notifications = []
-    def send_notification(self, user, notificaiton):
+        
+        
+    def send_notification(self, user, notification):
         self.notifications.append({"user": user, "notification": notification})
-        print(f"Notification sent to {user}: {notificaiton}")
+        print(f"Notification sent to {user}: {notification}")
+
     def get_notifications(self, user):
-    return [notif for notif in self.notifications if notif["user"] == user]
+        return [notif for notif in self.notifications if notif["user"] == user]
+
+
+
