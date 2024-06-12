07411a7b71c71c905cc4b94a2c9b737a1ed8d247
Hamish <24396@students.mrgs.school.nz>
2024-06-12 14:39:07 +1200

    user management class

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import tkinter as tk
 import json
+import os
 from tkinter import ttk, messagebox
 from messaging import Messaging
 from announcements import Announcements
 from forums import DiscussionForums
 from appointments import AppointmentScheduling
 from notifications import PushNotifications
+from user_management import UserManagement
 
+
+class UserManagement:
+    def __init__(self):
+        self.file_path = 'users.json'
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as f:
+                json.dump({}, f)
+
+    def save_user(self, username, password):
+        with open(self.file_path, 'r') as f:
+            users = json.load(f)
+        if username in users:
+            return False
+        users[username] = password
+        with open(self.file_path, 'w') as f:
+            json.dump(users, f)
+        return True
+
+    def verify_user(self, username, password):
+        with open(self.file_path, 'r') as f:
+            users = json.load(f)
+        if username in users and users[username] == password:
+            return True
+        return False
 
 
     
